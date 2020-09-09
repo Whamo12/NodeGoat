b4cdf4f5bdb094d7de1b31208d058b37fccbcb97
Alejandro Saenz <alejandro.saenz@softrams.com>
2020-09-09 14:22:52 -0400

    initial commit of remediation removal

--- app/routes/allocations.js
+++ app/routes/allocations.js
@@ -1,23 +1,19 @@
-const AllocationsDAO = require("../data/allocations-dao").AllocationsDAO;
+const AllocationsDAO = require('../data/allocations-dao').AllocationsDAO;
 
-function AllocationsHandler (db) {
-    "use strict";
+function AllocationsHandler(db) {
+  'use strict';
 
-    const allocationsDAO = new AllocationsDAO(db);
+  const allocationsDAO = new AllocationsDAO(db);
 
-    this.displayAllocations = (req, res, next) => {
-        /*
-        // Fix for A4 Insecure DOR -  take user id from session instead of from URL param
-        const { userId } = req.session;
-        */
-        const {userId} = req.params;
-        const { threshold } = req.query
+  this.displayAllocations = (req, res, next) => {
+    const { userId } = req.params;
+    const { threshold } = req.query;
 
-        allocationsDAO.getByUserIdAndThreshold(userId, threshold, (err, allocations) => {
-            if (err) return next(err);
-            return res.render("allocations", { userId, allocations });
-        });
-    };
+    allocationsDAO.getByUserIdAndThreshold(userId, threshold, (err, allocations) => {
+      if (err) return next(err);
+      return res.render('allocations', { userId, allocations });
+    });
+  };
 }
 
 module.exports = AllocationsHandler;
